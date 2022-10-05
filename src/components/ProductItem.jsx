faa37f0a9be4b8f8f5b920184aeaa9f68a2fad5a
Daniel Gonzalez <daniel_glez94@hotmail.com>
2022-10-04 21:28:47 -0500

    useState hook added to Product Item component

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "@styles/ProductItem.scss";
 import addToCart from "@icons/bt_add_to_cart.svg";
 
 const ProductItem = () => {
+  const [cart, setCart] = useState('');
+  const handleClick = () => {
+    setCart('Hola Mundo!');
+  }
+
   return (
     <div className="ProductItem">
       <img
@@ -14,9 +19,10 @@ const ProductItem = () => {
           <p>$120,00</p>
           <p>Bike</p>
         </div>
-        <figure>
+        <figure onClick={handleClick}>
           <img src={addToCart} alt="add-to-cart-img" />
         </figure>
+        {cart}
       </div>
     </div>
   );
