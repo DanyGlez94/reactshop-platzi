a25ae0aaf74134543e4edd72a4b7dd6160928a75
Daniel Gonzalez <daniel_glez94@hotmail.com>
2022-10-08 19:06:30 -0500

    Finishing shopping cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "@styles/Header.scss";
 import Menu from "@components/Menu";
 import menuIcon from "@icons/icon_menu.svg";
 import logo from "@logos/logo_yard_sale.svg";
+import AppContext from "@context/AppContext";
 import shoppingCart from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const {
+    state: { cart },
+  } = useContext(AppContext);
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -48,11 +52,11 @@ const Header = () => {
           </li>
           <li className="navbar-shopping-cart">
             <img src={shoppingCart} alt="shopping cart" />
-            <div>2</div>
+            {cart.length > 0 && <div>{cart.length}</div>}
           </li>
         </ul>
       </div>
-      {toggle ? <Menu /> : null}
+      {toggle && <Menu />}
     </nav>
   );
 };
